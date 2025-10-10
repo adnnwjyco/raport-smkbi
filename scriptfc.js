f31d780c5558041477767c6edb4a78bbbfb4a644
adnnwjyco <saya.adnanid@gmail.com>
2025-10-10 21:45:03 +0700

    terakhir

--- scriptfc.js
+++ scriptfc.js
@@ -1,5 +1,107 @@
-let hubungi = document.getElementById('hubungi')
+var select_kanan = document.getElementById('select_kanan')
+var select_kiri = document.getElementById('select_kiri')
+var bulat_lantai_1 = document.getElementById('bulat_lantai_1')
+var bulat_lantai_2 = document.getElementById('bulat_lantai_2')
+var bulat_lantai_3 = document.getElementById('bulat_lantai_3')
+var bulat_lantai_4 = document.getElementById('bulat_lantai_4')
 
-hubungi.addEventListener('click', function() {
-    window.open('https://saweria.co/Purosaichi');
-}); 
+var code_lantai1 = document.getElementById('code_lantai1')
+var code_lantai2 = document.getElementById('code_lantai2')
+var code_lantai3 = document.getElementById('code_lantai3')
+var code_lantai4 = document.getElementById('code_lantai4')
+
+select_kanan.addEventListener('click', function() {
+    if (code_lantai1.textContent === '1') {
+        bulat_lantai_1.style.background = 'grey';
+        bulat_lantai_2.style.background = 'blue';
+
+        setTimeout(function() {
+        code_lantai1.textContent = '0';
+        code_lantai2.textContent = '1';
+        }, 100);
+        
+    }
+
+    if (code_lantai2.textContent === '1') {
+        bulat_lantai_2.style.background = 'grey';
+        bulat_lantai_3.style.background = 'blue';
+
+        setTimeout(function() {
+        code_lantai2.textContent = '0';
+        code_lantai3.textContent = '1';
+        }, 100);
+
+    }
+
+    if (code_lantai3.textContent === '1') {
+        bulat_lantai_3.style.background = 'grey';
+        bulat_lantai_4.style.background = 'blue';
+
+        setTimeout(function() {
+        code_lantai3.textContent = '0';
+        code_lantai4.textContent = '1';
+    }, 100); 
+
+    }
+
+    if (code_lantai4.textContent === '1') {
+        bulat_lantai_4.style.background = 'grey';
+        bulat_lantai_1.style.background = 'blue';
+
+        setTimeout(function() {
+        code_lantai4.textContent = '0';
+        code_lantai1.textContent = '1';
+    }, 100); 
+
+    }
+
+});
+
+
+
+select_kiri.addEventListener('click', function() {
+    if (code_lantai1.textContent === '1') {
+        bulat_lantai_1.style.background = 'grey';
+        bulat_lantai_4.style.background = 'blue';
+
+        setTimeout(function() {
+        code_lantai1.textContent = '0';
+        code_lantai4.textContent = '1';
+        }, 100);
+        
+    }
+
+    if (code_lantai2.textContent === '1') {
+        bulat_lantai_2.style.background = 'grey';
+        bulat_lantai_1.style.background = 'blue';
+
+        setTimeout(function() {
+        code_lantai2.textContent = '0';
+        code_lantai1.textContent = '1';
+        }, 100);
+
+    }
+
+    if (code_lantai3.textContent === '1') {
+        bulat_lantai_3.style.background = 'grey';
+        bulat_lantai_2.style.background = 'blue';
+
+        setTimeout(function() {
+        code_lantai3.textContent = '0';
+        code_lantai2.textContent = '1';
+    }, 100); 
+
+    }
+
+    if (code_lantai4.textContent === '1') {
+        bulat_lantai_4.style.background = 'grey';
+        bulat_lantai_3.style.background = 'blue';
+
+        setTimeout(function() {
+        code_lantai4.textContent = '0';
+        code_lantai3.textContent = '1';
+    }, 100); 
+
+    }
+
+})
